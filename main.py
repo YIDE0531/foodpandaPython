7c4be464d01911c9ada02e795ec741ccef9e5fc9
s5846283 <s58462831@gmail.com.tw>
2020-05-28 17:09:18 +0800

    13

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import modules.file_funtion as fileFun
 import base64
 
-responseMsg = fileFun.webCrawlerRead()
+#responseMsg = fileFun.webCrawlerRead()
 
 #print (base64.b64encode(responseMsg.encode('utf-8')).decode('utf-8'))
 from flask import Flask
@@ -9,7 +9,8 @@ app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return base64.b64encode(responseMsg.encode('utf-8')).decode('utf-8')
+    return "base64.b64encode(responseMsg.encode('utf-8')).decode('utf-8')"
+    #return base64.b64encode(responseMsg.encode('utf-8')).decode('utf-8')
 
 @app.route("/test")
 def test():
