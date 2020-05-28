8aa7ad8953de46ad3e33bf0f7c1e4dcfd0415a43
s5846283 <s58462831@gmail.com.tw>
2020-05-28 17:06:47 +0800

    123

--- main.py
+++ main.py
@@ -3,4 +3,17 @@ import base64
 
 responseMsg = fileFun.webCrawlerRead()
 
-print (base64.b64encode(responseMsg.encode('utf-8')).decode('utf-8'))
+#print (base64.b64encode(responseMsg.encode('utf-8')).decode('utf-8'))
+from flask import Flask
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return base64.b64encode(responseMsg.encode('utf-8')).decode('utf-8')
+
+@app.route("/test")
+def test():
+    return "Hellow False8888888888"
+
+if __name__=="__main__":
+    app.run()
