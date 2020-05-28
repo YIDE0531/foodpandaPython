e29927804f080b83bd6f5fc86ae6b71dbe4320ed
YIDE0531 <54140752+YIDE0531@users.noreply.github.com>
2020-05-28 14:53:51 +0800

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import sys
-import base64
-
-ENCODING = sys.stdout.encoding if sys.stdout.encoding else 'utf-8'
-#print ('{ "id": 1, \"name": "林喔喔喔", \"price": 12.50, \"tags": ["home", "green"]} ')
-
-#print (('{ "id": 1, \"name": "林喔喔喔", \"price": 12.50, \"tags": ["home", "green"]}').encode("utf-8"))
-a = '林宜德! 666!'
-print (base64.b64encode(a.encode('utf-8')).decode('utf-8'))
